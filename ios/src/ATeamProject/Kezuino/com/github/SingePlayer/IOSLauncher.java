a565b7c8795d6f760f1bf37abe47d14861dae8e1
Anton van Dijk <anton.van.dijk@live.nl>
2015-03-16 15:40:31 +0100

    Update IOS launcher

--- ios/src/ATeamProject/Kezuino/com/github/SingePlayer/IOSLauncher.java
+++ ios/src/ATeamProject/Kezuino/com/github/SingePlayer/IOSLauncher.java
@@ -1,11 +1,11 @@
 package ATeamProject.Kezuino.com.github.SingePlayer;
 
+import ATeamProject.Kezuino.com.github.PactaleGame;
 import org.robovm.apple.foundation.NSAutoreleasePool;
 import org.robovm.apple.uikit.UIApplication;
 
 import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
-import ATeamProject.Kezuino.com.github.SingePlayer.PactaleGame;
 
 public class IOSLauncher extends IOSApplication.Delegate {
     @Override
