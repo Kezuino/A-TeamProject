436a333ee79390725b490934d3cba34abba11f83
Fatih Taskent <fatih-taskent@hotmail.com>
2015-03-18 12:42:28 +0100

    wall specificatie & UnitTestWall

---
+++ core/src/test/java/ateamproject/kezuino/com/github/singleplayer/TestWall.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ateamproject.kezuino.com.github.singleplayer;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Fatih
+ */
+public class TestWall {
+    
+    @Test
+    public void constructorValidation() {
+        
+        Map map = new Map(10,10);
+        Node node = new Node(map, 1, 1);
+        
+        /**
+	 * Initializes a wall at the position of the given node.
+	 * @param node
+	 */
+        
+        Wall wall = new Wall(node);
+        
+    }
+    
+    @Test
+    public void TestGetPortal() {
+        
+    }
+    
+    @Test
+    public void TestSetPortal() {
+        
+    }
+    
+    @Test
+    public void RemovePortal() {
+        
+    }
+    
+    @Test
+    public void TestGetPortals() {
+        
+    }
+}
