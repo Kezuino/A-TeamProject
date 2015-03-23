48d0b1fd62b328a4dbba2e92f140e0520464b849
Kezuino <ken.linde@outlook.com>
2015-03-23 15:56:43 +0100

    Implemented unittests Node skelet

--- core/src/main/java/ateamproject/kezuino/com/github/render/orthographic/camera/Camera.java
+++ core/src/main/java/ateamproject/kezuino/com/github/render/orthographic/camera/Camera.java
@@ -6,12 +6,12 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 
 public class Camera extends OrthographicCamera {
 
-    private float boundsXLower;
-    private float boundsXUpper;
-    private float boundsYLower;
-    private float boundsYUpper;
-    private float maxZoom;
-    private float minZoom;
+    private final float boundsXLower;
+    private final float boundsXUpper;
+    private final float boundsYLower;
+    private final float boundsYUpper;
+    private final float maxZoom;
+    private final float minZoom;
 
     public Camera(float viewportWidth, float viewportHeight, Map map, float scale) {
         super(viewportWidth, viewportHeight);
