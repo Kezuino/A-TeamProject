7f8b9cb280f40df3d47751bcab5a907b27931c5b
Kezuino <ken.linde@outlook.com>
2015-03-23 12:57:59 +0100

    Unittests for GameSession and Score, aswell as minor changes

--- core/src/main/java/ateamproject/kezuino/com/github/singleplayer/Score.java
+++ core/src/main/java/ateamproject/kezuino/com/github/singleplayer/Score.java
@@ -3,10 +3,14 @@ package ateamproject.kezuino.com.github.singleplayer;
 public class Score {
 
     private int score;
-    private GameSession GameSession;
+    private GameSession gameSession;
 
     public int getScore() {
         return this.score;
+    }
+    
+    public GameSession getGameSession() {
+        return this.gameSession;
     }
 
     public void setScore(int score) {
@@ -15,10 +19,11 @@ public class Score {
 
     /**
      * Initializes a score.
+     * 
+     * @param session The gamesession of this score
      */
-    public Score() {
-        // TODO - implement Score.Score
-        throw new UnsupportedOperationException();
+    public Score(GameSession session) {
+        this.gameSession = session;
     }
 
 }
