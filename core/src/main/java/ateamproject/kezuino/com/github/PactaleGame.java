8dbdd89a821099ee4f71fd384a24280c22321280
jip <jip.vandevijfeijke@gmail.com>
2015-03-25 16:44:15 +0100

    made changes to the game to be able to test the pathfinding part.

--- core/src/main/java/ateamproject/kezuino/com/github/PactaleGame.java
+++ core/src/main/java/ateamproject/kezuino/com/github/PactaleGame.java
@@ -1,10 +1,12 @@
 package ateamproject.kezuino.com.github;
 
+import ateamproject.kezuino.com.github.pathfinding.GameObjectPathfinding;
 import ateamproject.kezuino.com.github.render.IRenderer;
 import ateamproject.kezuino.com.github.render.orthographic.GameRenderer;
 import ateamproject.kezuino.com.github.render.screens.GameScreen;
 import ateamproject.kezuino.com.github.render.screens.LoginScreen;
 import ateamproject.kezuino.com.github.singleplayer.GameSession;
+import ateamproject.kezuino.com.github.singleplayer.Map;
 import ateamproject.kezuino.com.github.utility.Assets;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
@@ -15,7 +17,16 @@ public class PactaleGame extends Game {
 
     @Override
     public void create() {
-        this.setScreen(new LoginScreen(this));
+        //this.setScreen(new LoginScreen(this));
+        GameObjectPathfinding g = new GameObjectPathfinding();
+        GameSession gs = new GameSession(10);
+        gs.getMap().getNode(6, 5).setWall();
+        gs.getMap().getNode(5, 5).setWall();
+        gs.getMap().getNode(4, 5).setWall();
+        gs.getMap().getNode(3, 5).setWall();
+        gs.getMap().getNode(8, 5).setWall();
+        g.generatePath(gs.getMap().getNode(7, 7), gs.getMap().getNode(8, 2));
+
     }
 
     @Override
