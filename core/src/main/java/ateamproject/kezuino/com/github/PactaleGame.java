416896ed7b0c3fb0cd23e092a94b11594ecb10c9
jip <jip.vandevijfeijke@gmail.com>
2015-03-25 16:44:15 +0100

    made changes to the game to be able to test the pathfinding part.

--- core/src/main/java/ateamproject/kezuino/com/github/PactaleGame.java
+++ core/src/main/java/ateamproject/kezuino/com/github/PactaleGame.java
@@ -17,7 +17,16 @@ public class PactaleGame extends Game {
 
     @Override
     public void create() {
-        this.setScreen(new LoginScreen(this));
+        //this.setScreen(new LoginScreen(this));
+        GameObjectPathfinding g = new GameObjectPathfinding();
+        GameSession gs = new GameSession(10);
+        gs.getMap().getNode(6, 5).setWall();
+        gs.getMap().getNode(5, 5).setWall();
+        gs.getMap().getNode(4, 5).setWall();
+        gs.getMap().getNode(3, 5).setWall();
+        gs.getMap().getNode(8, 5).setWall();
+        g.generatePath(gs.getMap().getNode(7, 7), gs.getMap().getNode(8, 2));
+
     }
 
     @Override
