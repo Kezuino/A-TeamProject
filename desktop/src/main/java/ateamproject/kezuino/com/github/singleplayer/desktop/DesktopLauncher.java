a2452b1a8c71d540649616e6283660323c34adfa
jip <jip.vandevijfeijke@gmail.com>
2015-03-28 12:14:34 +0100

    updates pathfinding, still buggy

--- desktop/src/main/java/ateamproject/kezuino/com/github/singleplayer/desktop/DesktopLauncher.java
+++ desktop/src/main/java/ateamproject/kezuino/com/github/singleplayer/desktop/DesktopLauncher.java
@@ -1,6 +1,8 @@
 package ateamproject.kezuino.com.github.singleplayer.desktop;
 
 import ateamproject.kezuino.com.github.PactaleGame;
+import ateamproject.kezuino.com.github.pathfinding.GameObjectPathfinding;
+import ateamproject.kezuino.com.github.singleplayer.GameSession;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
@@ -10,6 +12,13 @@ public class DesktopLauncher {
         cfg.foregroundFPS = 60;
         cfg.backgroundFPS = 30;
         cfg.samples = 8;
+        
+            GameObjectPathfinding p = new GameObjectPathfinding();
+            GameSession g = new GameSession();
+            g.setMap(3);
+            g.getMap().getNode(0, 1).setWall();
+            g.getMap().getNode(1, 1).setWall();
+            p.generatePath(g.getMap().getNode(0, 0), g.getMap().getNode(2, 2));
 
 		new LwjglApplication(new PactaleGame(), cfg);
 	}
