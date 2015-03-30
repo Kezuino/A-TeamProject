128aef7593cc37bdb6e332854199f9fe6e8741bb
Fatih Taskent <fatih-taskent@hotmail.com>
2015-03-30 13:52:15 +0200

    ClanGames gui with correct viewport

---
+++ core/src/main/java/ateamproject/kezuino/com/github/render/screens/ClanGames.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ateamproject.kezuino.com.github.render.screens;
+
+import ateamproject.kezuino.com.github.render.IRenderer;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+
+/**
+ * @author David
+ */
+public class LoginScreen implements Screen {
+    private Game game;
+    private Stage stage;
+    
+    public LoginScreen(Game game){
+        this.game = game;
+        stage = new Stage();
+        Gdx.input.setInputProcessor(stage);
+        Skin skin = new Skin(Gdx.files.internal("uiskin.json"));
+
+        // TODO: Redesign login screen to match documentation.
+        TextButton btnLogin = new TextButton("Press me!", skin);
+        btnLogin.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                game.setScreen(new GameScreen(game));
+            }
+        });
+
+        btnLogin.setSize(300, 40);
+        btnLogin.setPosition(stage.getWidth()/2-btnLogin.getWidth()/2, stage.getHeight()/2-btnLogin.getHeight()/2);
+
+        stage.addActor(btnLogin);
+    }
+    
+    @Override
+    public void show() {
+        // Initialize screen here.
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 0);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        
+        stage.act(delta);
+        stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        // Reset controls of this screen to align with new resolution.
+    }
+
+    @Override
+    public void pause() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void resume() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void hide() {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void dispose() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
