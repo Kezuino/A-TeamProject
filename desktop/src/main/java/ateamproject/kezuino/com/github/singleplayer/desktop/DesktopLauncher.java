ab3c3da04a556cb34b4e5f74f1b1c2d081ed6c9c
Anton van Dijk <anton.van.dijk@live.nl>
2015-04-03 23:32:03 +0200

    Added quick framerate test method to desktop launcher.

--- desktop/src/main/java/ateamproject/kezuino/com/github/singleplayer/desktop/DesktopLauncher.java
+++ desktop/src/main/java/ateamproject/kezuino/com/github/singleplayer/desktop/DesktopLauncher.java
@@ -11,8 +11,21 @@ public class DesktopLauncher {
         LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
         cfg.foregroundFPS = 60;
         cfg.backgroundFPS = 30;
+
         //cfg.samples = 8;
 
+        //frameRateTest(cfg);
+
         new LwjglApplication(new PactaleGame(), cfg);
+    }
+
+    public static void frameRateTest(LwjglApplicationConfiguration cfg) {
+        cfg.foregroundFPS = 0;
+        cfg.backgroundFPS = 0;
+        cfg.width = 1280;
+        cfg.height = 720;
+        cfg.fullscreen = false;
+        cfg.forceExit = true;
+        cfg.vSyncEnabled = false;
     }
 }
