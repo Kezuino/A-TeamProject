d0c11287b897719442240554a76b86c655bab154
Anton van Dijk <anton.van.dijk@live.nl>
2015-04-03 16:47:16 +0200

    Removed duplicated MainMenu. Renamed ClanGamesScreen

---
+++ core/src/main/java/ateamproject/kezuino/com/github/render/screens/ClanGamesScreen.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ateamproject.kezuino.com.github.render.screens;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import javafx.scene.paint.Color;
+
+/**
+ * @author David
+ */
+public class ClanGamesScreen extends BaseScreen{
+
+    public ClanGamesScreen(Game game)
+    {
+        super(game);
+    
+        Table table = new Table();
+       
+        for (int i = 0; i < 5; i++) {
+            Label clan = new Label("OurClan", skin);        
+            Label clanjoin = new Label("8/10", skin);
+            
+            table.add(clan);
+            table.add(clanjoin);
+            table.row();
+        }
+        
+        TextButton btnBack = new TextButton("< Back", skin);
+         btnBack.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                game.setScreen(new GameScreen(game));
+            }
+        });
+        btnBack.setSize(300, 400);
+        
+        table.add(btnBack).colspan(2);
+        
+        
+        table.setPosition(stage.getWidth()/2, stage.getHeight()/2);
+        
+        //stage.addActor(btnLogin);        
+        stage.addActor(table);
+
+    }    
+ 
+
+    /*@Override
+    public void resize(int width, int height) {
+        // Reset controls of this screen to align with new resolution.
+        viewport.update(width, height);
+    }*/
+
+}
