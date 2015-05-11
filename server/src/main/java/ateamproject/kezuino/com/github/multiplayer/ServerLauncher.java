d482587de35ecb5c8971b2e2270af0f0413ea4c5
Anton van Dijk <anton.van.dijk@live.nl>
2015-05-11 02:30:20 +0200

    Added server module.
    Server module will be the dedicated server program to run on the server
    computer.

---
+++ server/src/main/java/ateamproject/kezuino/com/github/multiplayer/ServerLauncher.java
@@ -0,0 +1,8 @@
+package ateamproject.kezuino.com.github.multiplayer;
+
+public class ServerLauncher {
+    public static void main(String[] args) {
+        // TODO: Setup RMI and start listening for commands.
+        System.out.println("Server launched!");
+    }
+}
