c1013ef6f87f07f5c90d38a426beb115ecb1d0ac
Svenke <svenkeunen@hotmail.com>
2015-05-20 14:18:32 +0200

    RMI Methoden added
    RMI Methoden added

--- server/src/main/java/ateamproject/kezuino/com/github/multiplayer/Server.java
+++ server/src/main/java/ateamproject/kezuino/com/github/multiplayer/Server.java
@@ -1,6 +1,7 @@
 package ateamproject.kezuino.com.github.multiplayer;
 
 import ateamproject.kezuino.com.github.network.rmi.IProtocolServer;
+import ateamproject.kezuino.com.github.render.screens.ClanFunctions;
 
 import java.rmi.AlreadyBoundException;
 import java.rmi.RemoteException;
@@ -10,12 +11,15 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
 
 public class Server extends UnicastRemoteObject implements IProtocolServer {
     private transient static Server currentInstance;
+    private ClanFunctions clanFunctions;
 
     protected Server() throws RemoteException {
         super(Registry.REGISTRY_PORT);
+        clanFunctions = new ClanFunctions();
     }
     
     public static Server instance() throws RemoteException {
@@ -58,5 +62,56 @@ public class Server extends UnicastRemoteObject implements IProtocolServer {
     @Override
     public void creatureMove(int creatureID) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<String> clanFillTable(String emailadres) throws RemoteException {
+        return clanFunctions.fillTable(emailadres);
+    }
+
+    @Override
+    public boolean clanCreateClan(String clanName, String emailaddress) throws RemoteException {
+        return clanFunctions.createClan(clanName, emailaddress);
+    }
+
+
+    @Override
+    public ClanFunctions.invitationType clanGetInvitation(String clanName, String emailaddress) throws RemoteException {
+        return clanFunctions.getInvitation(clanName, emailaddress);
+    }
+
+    @Override
+    public ClanFunctions.managementType getManagement(String clanName, String emailaddress) throws RemoteException {
+        return clanFunctions.getManagement(clanName, emailaddress);
+    }
+
+    @Override
+    public String getPersons(String clanName) throws RemoteException {
+       return clanFunctions.getPersons(clanName);
+    }
+
+    @Override
+    public boolean handleInvitation(ClanFunctions.invitationType invite, String clanName, String emailAddress, String nameOfInvitee) throws RemoteException {
+       return clanFunctions.handleInvitation(invite, clanName, emailAddress, nameOfInvitee);
+    }
+
+    @Override
+    public boolean handleManagement(ClanFunctions.managementType manage, String clanName, String emailaddress) throws RemoteException {
+        return clanFunctions.handleManagement(manage, clanName, emailaddress);
+    }
+
+    @Override
+    public String getUsername(String emailaddress) throws RemoteException {
+       return clanFunctions.getUsername(emailaddress);
+    }
+
+    @Override
+    public String getEmail(String username) throws RemoteException {
+        return clanFunctions.getEmail(username);
+    }
+
+    @Override
+    public boolean setUsername(String name, String emailaddress) throws RemoteException {
+       return clanFunctions.setUsername(name, emailaddress);
     }
 }
