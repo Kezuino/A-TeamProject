b693abcf75b93abdf4e3a699da90a3113440099e
Fatih Taskent <fatih-taskent@hotmail.com>
2015-05-20 22:56:31 +0200

    lobbylistscreen loads games from server

--- core/src/main/java/ateamproject/kezuino/com/github/render/screens/LobbyListScreen.java
+++ core/src/main/java/ateamproject/kezuino/com/github/render/screens/LobbyListScreen.java
@@ -5,16 +5,22 @@
  */
 package ateamproject.kezuino.com.github.render.screens;
 
+import ateamproject.kezuino.com.github.network.rmi.Client;
+import ateamproject.kezuino.com.github.network.rmi.Lobby;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
-
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import java.rmi.RemoteException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * @author Fatih
@@ -36,6 +42,12 @@ public class LobbyListScreen extends BaseScreen {
         scrollTable.clear();
 
         TextButton btnCreateGame = new TextButton("Maak spel", skin);
+        btnCreateGame.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                game.setScreen(new LobbyScreen(game,true));
+            }
+        });
         btnCreateGame.setPosition(stage.getWidth() - btnCreateGame.getWidth() - 10, stage.getHeight() - btnCreateGame.getHeight() - 10);
         this.stage.addActor(btnCreateGame);
 
@@ -87,19 +99,33 @@ public class LobbyListScreen extends BaseScreen {
 
     private void FillHostTable() {
         List<String[]> hostList;
+        ArrayList<String[]> hostList;
+        
+        Client client;
+        try {
+            client = Client.instance();
+            hostList = client.getLobbies();
+        } catch (RemoteException ex) {
+            Logger.getLogger(LobbyListScreen.class.getName()).log(Level.SEVERE, null, ex);
+        }
+                
+                
+        /*
+        
         if (!clanGame) {
             hostList = LobbyFunctions.getRandomHostList();
         } else {
             hostList = LobbyFunctions.getClanHostList();
         }
+       */
 
         for (int i = 0; i < hostList.size(); i++) {
 
-            TextField lb1 = new TextField(hostList.get(i)[0], skin);
+            TextField lb1 = new TextField(hostList.get(i).getLobbyName(), skin);
             lb1.setDisabled(true);
-            TextField lb2 = new TextField(hostList.get(i)[1], skin);
+            TextField lb2 = new TextField(hostList.get(i).getLobbyId().toString(), skin);
             lb2.setDisabled(true);
-            TextField lb3 = new TextField(hostList.get(i)[2], skin);
+            TextField lb3 = new TextField(Integer.toString(hostList.get(i).getMembers().size()), skin);
             lb3.setDisabled(true);
             TextButton btnJoin = new TextButton("Join", skin);
             btnJoin.setDisabled(true);
