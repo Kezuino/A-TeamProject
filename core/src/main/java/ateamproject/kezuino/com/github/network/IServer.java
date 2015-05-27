69505ba4fc82b8a10be47d133fe41308d7258b0c
Anton van Dijk <anton.van.dijk@live.nl>
2015-05-27 12:29:11 +0200

    Updated server logic

--- core/src/main/java/ateamproject/kezuino/com/github/network/IServer.java
+++ core/src/main/java/ateamproject/kezuino/com/github/network/IServer.java
@@ -1,7 +1,5 @@
 package ateamproject.kezuino.com.github.network;
 
-import ateamproject.kezuino.com.github.network.packet.Packet;
-
 public interface IServer {
     /**
      * Starts up this {@link IServer} listening on connections to this computer on a port.
@@ -12,11 +10,4 @@ public interface IServer {
      * Stops this {@link IServer} from listening to clients and drops any active connections. {@link IServer} can be started up again using {@link #start()}.
      */
     void stop();
-
-    /**
-     * Sends a {@link Packet} over the network.
-     *
-     * @param packet
-     */
-    void send(Packet packet);
 }
