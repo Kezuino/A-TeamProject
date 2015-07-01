1261a9d0757361f711e550f49acbecf0e9c2fbed
jip <jip.vandevijfeijke@gmail.com>
2015-07-01 14:39:36 +0200

    updated param description

--- run_client.bat
+++ run_client.bat
@@ -1,5 +1,5 @@
 @echo off
 
-REM 1st param is the IP of the server
-java -jar "%~dp0/desktop/build/libs/desktop-1.0.jar" localhost
+REM 1st param is the ip of the client itself. second param is the IP of the server
+java -jar "%~dp0/desktop/build/libs/desktop-1.0.jar" localhost localhost
 pause
